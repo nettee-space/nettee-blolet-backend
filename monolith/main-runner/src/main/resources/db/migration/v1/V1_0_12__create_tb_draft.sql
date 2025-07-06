33d57685331c75a8b484200090437aed4f4c3150
merge-simpson <letsdev@kakao.com>
2025-07-07 07:17:38 +0900

    fix(main-runner): add path column in draft DDL

--- monolith/main-runner/src/main/resources/db/migration/v1/V1_0_12__create_tb_draft.sql
+++ monolith/main-runner/src/main/resources/db/migration/v1/V1_0_12__create_tb_draft.sql
@@ -8,20 +8,23 @@ CREATE TABLE IF NOT EXISTS article.draft (
     title       VARCHAR(255),
     content     TEXT,
     -- content     JSONB               --JSON 검토중
+    "path"      VARCHAR(255),
 
     status      INTEGER,
     created_at  TIMESTAMP   DEFAULT NOW()   NOT NULL,
     updated_at  TIMESTAMP   DEFAULT NOW()   NOT NULL
-
-    );
+);
 
 -- 컬럼 코멘트
+COMMENT ON COLUMN article.draft.id          IS '임시 게시물 PK';
+COMMENT ON COLUMN article.draft.blog_id     IS '블로그 ID';
+COMMENT ON COLUMN article.draft.article_id  IS '포스트 ID';
 COMMENT ON COLUMN article.draft.title       IS '글 제목';
 COMMENT ON COLUMN article.draft.content     IS '내용';
 COMMENT ON COLUMN article.draft.status      IS '상태';
+COMMENT ON COLUMN article.draft."path"      IS '게시물 URL path';
 COMMENT ON COLUMN article.draft.created_at  IS '생성시간';
 COMMENT ON COLUMN article.draft.updated_at  IS '마지막 수정시간';
-COMMENT ON COLUMN article.draft.blog_id     IS '블로그 ID';
-COMMENT ON COLUMN article.draft.article_id  IS '포스트 ID';
 
 ALTER TABLE "article"."draft" ADD CONSTRAINT "pk_draft" PRIMARY KEY ("id");
+ALTER TABLE "article"."draft" ADD CONSTRAINT "uq_draft_path" UNIQUE ("blog_id", "path");
