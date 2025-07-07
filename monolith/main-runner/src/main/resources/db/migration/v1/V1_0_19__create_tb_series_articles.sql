fb75e18302c8e74f063978a2f534220a995c6df7
merge-simpson <letsdev@kakao.com>
2025-07-07 22:00:48 +0900

    fix(main-runner): add article_id column to series_article DDL to denormalize

--- monolith/main-runner/src/main/resources/db/migration/v1/V1_0_19__create_tb_series_articles.sql
+++ monolith/main-runner/src/main/resources/db/migration/v1/V1_0_19__create_tb_series_articles.sql
@@ -4,6 +4,7 @@ CREATE SCHEMA IF NOT EXISTS "series";
 -- Schema: series
 CREATE TABLE IF NOT EXISTS "series"."series_article" (
     "series_id"         BIGINT,
+    "aricle_id"         BIGINT, -- 비정규화
     "draft_id"          BIGINT,
     "display_order"     INTEGER,
     "created_at"        TIMESTAMP       DEFAULT NOW(),
@@ -14,8 +15,12 @@ CREATE TABLE IF NOT EXISTS "series"."series_article" (
 COMMENT ON TABLE "series"."series_articles" IS '시리즈아티클';
 
 -- 컬럼 코멘트
-COMMENT ON COLUMN "series"."series_article"."display_order"    IS '사용자정렬순서';
-COMMENT ON COLUMN "series"."series_article"."created_at"       IS '생성시간';
-COMMENT ON COLUMN "series"."series_article"."updated_at"       IS '마지막 수정시간';
+COMMENT ON COLUMN "series"."series_article"."series_id"         IS '시리즈 PK';
+COMMENT ON COLUMN "series"."series_article"."aricle_id"         IS '아티클 PK';
+COMMENT ON COLUMN "series"."series_article"."draft_id"          IS '임시아티클 PK';
+COMMENT ON COLUMN "series"."series_article"."display_order"     IS '사용자정렬순서';
+COMMENT ON COLUMN "series"."series_article"."created_at"        IS '생성시간';
+COMMENT ON COLUMN "series"."series_article"."updated_at"        IS '마지막 수정시간';
 
+ALTER TABLE "series"."series_article" ADD CONSTRAINT "uq_series_articles_series_id_article_id" UNIQUE ("series_id", "article_id");
 ALTER TABLE "series"."series_article" ADD CONSTRAINT "uq_series_articles_series_id_draft_id" UNIQUE ("series_id", "draft_id");
