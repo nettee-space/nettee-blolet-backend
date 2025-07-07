6dd094ef92d31b6f5bed6021f74bc5341a2bac24
merge-simpson <letsdev@kakao.com>
2025-07-07 10:37:40 +0900

    fix(main-runner): rename `user_sort_order` column to `display_order` in series DDL

--- monolith/main-runner/src/main/resources/db/migration/v1/V1_0_18__create_tb_series.sql
+++ monolith/main-runner/src/main/resources/db/migration/v1/V1_0_18__create_tb_series.sql
@@ -6,18 +6,20 @@ CREATE TABLE IF NOT EXISTS "series"."series" (
     "id"          BIGINT,
     "blog_id"     BIGINT,
     "title"       VARCHAR(255),
-    "user_sort_order"  INTEGER,
+    "display_order"  INTEGER,
     "created_at"  TIMESTAMP       DEFAULT NOW(),
     "updated_at"  TIMESTAMP
-    );
+);
 
 --테이블 코멘트
 COMMENT ON TABLE "series"."series" IS '시리즈';
 
 -- 컬럼 코멘트
-COMMENT ON COLUMN "series"."series"."title"               IS '시리즈제목';
-COMMENT ON COLUMN "series"."series"."user_sort_order"     IS '사용자정렬순서';
-COMMENT ON COLUMN "series"."series"."created_at"          IS '생성시간';
-COMMENT ON COLUMN "series"."series"."updated_at"          IS '마지막 수정시간';
+COMMENT ON COLUMN "series"."series"."id"                IS '시리즈 PK';
+COMMENT ON COLUMN "series"."series"."blog_id"           IS '블로그 PK';
+COMMENT ON COLUMN "series"."series"."title"             IS '시리즈제목';
+COMMENT ON COLUMN "series"."series"."display_order"     IS '사용자정렬순서';
+COMMENT ON COLUMN "series"."series"."created_at"        IS '생성시간';
+COMMENT ON COLUMN "series"."series"."updated_at"        IS '마지막 수정시간';
 
 ALTER TABLE "series"."series" ADD CONSTRAINT "pk_series" PRIMARY KEY ("id");
