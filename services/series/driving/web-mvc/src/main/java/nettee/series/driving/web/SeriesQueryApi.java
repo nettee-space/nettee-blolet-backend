3dd2b589ec040b17c4622e57c1e6c53df71ea2e5
silberbullet <pkw961028@naver.com>
2025-07-10 17:59:41 +0900

    feat(series): add series query api

--- services/series/driving/web-mvc/src/main/java/nettee/series/driving/web/SeriesQueryApi.java
+++ services/series/driving/web-mvc/src/main/java/nettee/series/driving/web/SeriesQueryApi.java
@@ -1,5 +1,12 @@
 package nettee.series.driving.web;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "Series Query API")
 public class SeriesQueryApi {
 
 }
