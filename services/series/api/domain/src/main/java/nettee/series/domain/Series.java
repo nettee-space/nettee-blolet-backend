06ab333261c3292c0fa374a9a9cbba7e65f0f147
silberbullet <pkw961028@naver.com>
2025-07-13 17:34:15 +0900

    feat(series): add description and banner column and series article List

--- services/series/api/domain/src/main/java/nettee/series/domain/Series.java
+++ services/series/api/domain/src/main/java/nettee/series/domain/Series.java
@@ -39,11 +39,13 @@ public class Series {
             builderMethodName = "prepareUpdate",
             buildMethodName = "update"
     )
-    public void update(String title, Integer displayOrder) {
+    public void update(String title, Integer displayOrder, String description, byte[] banner) {
         Objects.requireNonNull(title, "Title cannot be null");
         Objects.requireNonNull(displayOrder, "DisplayOrder cannot be null");
         
         this.title = title;
+        this.description = description;
+        this.banner = banner;
         this.displayOrder = displayOrder;
         this.updatedAt = Instant.now();
     }
