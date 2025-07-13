4a03b4cb5745265dbfcabbf47e650451ac617700
silberbullet <pkw961028@naver.com>
2025-07-13 19:21:53 +0900

    feat(series): add series detail read model

--- services/series/api/readmodel/src/main/java/nettee/series/readmodel/SeriesQueryModels.java
+++ services/series/api/readmodel/src/main/java/nettee/series/readmodel/SeriesQueryModels.java
@@ -1,17 +1,32 @@
 package nettee.series.readmodel;
 
 import lombok.Builder;
+import nettee.series.article.readmodel.SeriesArticleQueryModels.SeriesArticleSummary;
 
 import java.time.Instant;
+import java.util.List;
 
 public final class SeriesQueryModels {
 
     private SeriesQueryModels() {}
-
+    
+    @Builder
+    public record SeriesDetail(
+            String id,
+            String blogId,
+            String title,
+            String description,
+            Byte[] banner,
+            List<SeriesArticleSummary> seriesArticleSummaryList,
+            Instant createdAt,
+            Instant updatedAt
+    ) {
+    }
+    
     @Builder
     public record SeriesSummary(
             String id,
-            String blogID,
+            String blogId,
             String title,
             Integer displayOrder,
             Instant createdAt,
