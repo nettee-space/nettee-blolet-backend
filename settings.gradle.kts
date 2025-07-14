ce5a2a246f608388ec4f5e1a02eca571aa0b24ca
wch-os <wch1224.s@gmail.com>
2025-07-15 00:06:35 +0900

    chore(auth): add minimal auth module for swagger docs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,6 @@ val services = "${rootProject.projectDir}/services"
 apply(from = "common/common.settings.gradle.kts")
 apply(from = "core/core.settings.gradle.kts")
 apply(from = "monolith/monolith.settings.gradle.kts")
+
+// services
+apply(from = "$services/auth/auth.settings.gradle.kts")
