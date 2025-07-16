3d6fdabebe1ee1d98ca72138b42bbe87706fef95
merge-simpson <letsdev@kakao.com>
2025-07-16 15:45:22 +0900

    feat(blog): add blog API controllers

---
+++ services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/BlogQueryApi.java
@@ -0,0 +1,44 @@
+package nettee.blolet.blog.web;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import nettee.blolet.blog.web.dto.BlogQueryDto.BlogDetailViewResponse;
+import nettee.blolet.blog.web.dto.BlogQueryDto.BlogListViewResponse;
+import nettee.blolet.blog.web.dto.BlogQueryDto.BlogOwnerProfileIds;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("blogs")
+@Tag(name = "Blog")
+public class BlogQueryApi {
+
+    /**
+     * TODO 예상되는 정책 또는 논의 (블로그 목록 조회)
+     * <ul>
+     *     <li>Q. 사용자는 여러 블로그를 가질 수 있을까요?</li>
+     * </ul>
+     */
+    @GetMapping
+    @Operation(
+            summary = "블로그 목록 조회 (여러 사용자)",
+            description = "선택한 사용자별 블로그 목록을 조회합니다."
+    )
+    public BlogListViewResponse findAllByUsernames(@RequestBody BlogOwnerProfileIds profileIds) {
+        return null;
+    }
+
+    @GetMapping("/{blogId}")
+    @Operation(
+            summary = "블로그 상세 조회",
+            description = "블로그 정보를 조회합니다."
+    )
+    public BlogDetailViewResponse findByBlogId(@PathVariable("blogId") String blogId) {
+        return null;
+    }
+}
