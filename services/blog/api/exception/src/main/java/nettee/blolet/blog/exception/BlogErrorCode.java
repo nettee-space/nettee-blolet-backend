6fcccd1651ef8946276342fc66a57a06034a126f
merge-simpson <letsdev@kakao.com>
2025-07-16 15:43:50 +0900

    feat(blog): add blog error code

---
+++ services/blog/api/exception/src/main/java/nettee/blolet/blog/exception/BlogErrorCode.java
@@ -0,0 +1,60 @@
+package nettee.blolet.blog.exception;
+
+import nettee.common.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+public enum BlogErrorCode implements ErrorCode {
+    BLOG_NOT_FOUND("블로그를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    DEFAULT("블로그 API 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    private final String message;
+    private final HttpStatus status;
+
+    BlogErrorCode(String message, HttpStatus status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+
+    @Override
+    public HttpStatus httpStatus() {
+        return status;
+    }
+
+    @Override
+    public RuntimeException exception() {
+        return new BlogException(this);
+    }
+
+    @Override
+    public RuntimeException exception(Throwable cause) {
+        return new BlogException(this, cause);
+    }
+
+    @Override
+    public RuntimeException exception(Runnable runnable) {
+        return new BlogException(this, runnable);
+    }
+
+    @Override
+    public RuntimeException exception(Runnable runnable, Throwable cause) {
+        return new BlogException(this, runnable, cause);
+    }
+
+    @Override
+    public RuntimeException exception(Supplier<Map<String, Object>> appendPayload) {
+        return new BlogException(this, appendPayload);
+    }
+
+    @Override
+    public RuntimeException exception(Supplier<Map<String, Object>> appendPayload, Throwable cause) {
+        return new BlogException(this, appendPayload, cause);
+    }
+}
