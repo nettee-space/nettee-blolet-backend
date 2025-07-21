60435b2d8ed887a9a27c261c24435de70b63b000
merge-simpson <letsdev@kakao.com>
2025-07-21 10:21:31 +0900

    feat(blog): add lombok annotations, update methods in domain

--- services/blog/api/domain/src/main/java/nettee/blolet/blog/domain/Blog.java
+++ services/blog/api/domain/src/main/java/nettee/blolet/blog/domain/Blog.java
@@ -1,7 +1,17 @@
 package nettee.blolet.blog.domain;
 
-import java.time.Instant;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+import java.util.Objects;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Blog {
     private String id;
     private String userId;
@@ -13,6 +23,25 @@ public class Blog {
     public Blog(String userId, String name, String url) {
         this.userId = userId;
         this.name = name;
+        this.url = url;
+    }
+
+    @Builder(
+            builderClassName = "DefaultBlogUpdateBuilder",
+            builderMethodName = "prepareUpdate",
+            buildMethodName = "update"
+    )
+    public void update(String name, String url) {
+        Objects.requireNonNull(name, "Name must not be null");
+
+        this.name = name;
+        if (url != null) {
+            this.url = url;
+        }
+    }
+
+    public void updateUrl(String url) {
+        Objects.requireNonNull(url, "Url must not be null");
         this.url = url;
     }
 }
