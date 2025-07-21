b27ac9f05a62b97a66dced4a1bd27e4ef36a5085
merge-simpson <letsdev@kakao.com>
2025-07-21 20:07:32 +0900

    feat(blog): add transaction(()->{}) in command port

--- services/blog/application/src/main/java/nettee/blolet/blog/application/port/BlogCommandPort.java
+++ services/blog/application/src/main/java/nettee/blolet/blog/application/port/BlogCommandPort.java
@@ -3,5 +3,7 @@ package nettee.blolet.blog.application.port;
 import nettee.blolet.blog.domain.Blog;
 
 public interface BlogCommandPort {
+    void transaction(Runnable runnable);
+
     Blog save(Blog blog);
 }
