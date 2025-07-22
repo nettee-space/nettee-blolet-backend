e2dea11b0e149b26b4609c1de1282bf84c831423
merge-simpson <letsdev@kakao.com>
2025-07-22 20:49:23 +0900

    test(blog): add test for exception on deleting non-existent blog
    mock deleteById to throw BLOG_NOT_FOUND exception and verify deleteById not called for wrong id

--- services/blog/application/src/test/kotlin/nettee/blolet/blog/application/service/BlogCommandServiceTest.kt
+++ services/blog/application/src/test/kotlin/nettee/blolet/blog/application/service/BlogCommandServiceTest.kt
@@ -98,6 +98,7 @@ class BlogCommandServiceTest : FreeSpec({
 
     "[DELETE] 블로그 삭제 시" - {
         val targetId = "1"
+        val wrongTargetId = "999"
 
         // mock:
         val now = Instant.now()
@@ -129,6 +130,27 @@ class BlogCommandServiceTest : FreeSpec({
             // assert
             capturedItems.size shouldBe firstSize - 1
             verify(exactly = 1) { commandPort.deleteById(targetId) }
+        }
+
+        "🚧 존재하지 않는 아이템을 삭제하려고 하면 예외를 반환한다." {
+            every { commandPort.deleteById(any()) } answers {
+                val inputtedId: String = firstArg()
+                val exists = capturedItems.map { it.id }.contains(inputtedId)
+                if (!exists) {
+                    throw BLOG_NOT_FOUND.exception()
+                }
+
+                capturedItems.removeIf { it.id == inputtedId }
+            }
+
+            // action & assert
+            val exception = shouldThrow<CustomException> {
+                commandService.deleteById(wrongTargetId)
+            }
+            exception.errorCode shouldBe BLOG_NOT_FOUND
+
+            // verify: port.deleteById 메서드는 실제로 호출되지 않았는지 확인.
+            verify(inverse = true) { commandPort.deleteById(targetId) }
         }
     }
 })
