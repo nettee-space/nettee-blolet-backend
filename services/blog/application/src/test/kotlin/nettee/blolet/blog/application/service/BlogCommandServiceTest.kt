1fc62c4db7efb8579ed364bf907aec14b98752fa
merge-simpson <letsdev@kakao.com>
2025-07-22 18:30:32 +0900

    test(blog): add test to throw blog maximum exceeded error when user exceeds blog creation limit
    use kotest shouldThrow to assert BLOG_MAXIMUM_EXCEEDED when countByUserId meets limit and mock save behavior

--- services/blog/application/src/test/kotlin/nettee/blolet/blog/application/service/BlogCommandServiceTest.kt
+++ services/blog/application/src/test/kotlin/nettee/blolet/blog/application/service/BlogCommandServiceTest.kt
@@ -1,11 +1,14 @@
 package nettee.blolet.blog.application.service
 
+import io.kotest.assertions.throwables.*
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.*
 import io.kotest.matchers.equals.*
 import io.mockk.*
 import nettee.blolet.blog.application.port.BlogCommandPort
 import nettee.blolet.blog.domain.Blog
+import nettee.blolet.blog.exception.BlogErrorCode.BLOG_MAXIMUM_EXCEEDED
+import nettee.common.CustomException
 import java.time.Instant
 
 class BlogCommandServiceTest : FreeSpec({
@@ -63,6 +66,32 @@ class BlogCommandServiceTest : FreeSpec({
             savedItem shouldBeEqual expectedItem
             // 모킹된 port.save(...) 함수가 1회만 호출되었는지 확인
             verify(exactly = 1) { commandPort.save(item) }
+        }
+
+        "🚧 사용자당 블로그 개설 개수를 초과하면 오류를 반환한다." {
+            // mock:
+            val capturedItem = mutableListOf<Blog>(item)
+            every { commandPort.countByUserId(any()) } answers {
+                val userIdArg: String = firstArg()
+                capturedItem.count {it.userId == userIdArg}
+            }
+            every { commandPort.save(capture(capturedItem)) } answers {
+                val inputtedBlog: Blog = firstArg()
+                Blog.builder()
+                    .id(inputtedBlog.id ?: "1")
+                    .userId(inputtedBlog.userId)
+                    .name(inputtedBlog.name)
+                    .url(inputtedBlog.url)
+                    .createdAt(inputtedBlog.createdAt)
+                    .updatedAt(inputtedBlog.createdAt)
+                    .build()
+            }
+
+            // action & assert:
+            val exception = shouldThrow<CustomException> {
+                commandService.save(item)
+            }
+            exception.errorCode shouldBe BLOG_MAXIMUM_EXCEEDED
         }
     }
 })
