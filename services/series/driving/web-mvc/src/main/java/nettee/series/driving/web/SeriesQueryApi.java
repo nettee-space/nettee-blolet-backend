d01fc0caf6d7b79030146b5efb0b1e99482d5382
silberbullet <pkw961028@naver.com>
2025-07-23 02:45:28 +0900

    feat(series): feat query api

--- services/series/driving/web-mvc/src/main/java/nettee/series/driving/web/SeriesQueryApi.java
+++ services/series/driving/web-mvc/src/main/java/nettee/series/driving/web/SeriesQueryApi.java
@@ -8,6 +8,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import nettee.series.application.usecase.SeriesReadUseCase;
+import nettee.series.driving.web.dto.SeriesQueryDto.SeriesDetailResponse;
 import nettee.series.driving.web.dto.SeriesQueryDto.SeriesSummaryResponse;
 import nettee.series.readmodel.SeriesQueryModels.SeriesSummary;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +17,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("series")
 @Tag(name = "Series", description = "Series API")
 public class SeriesQueryApi {
+    
+    private final SeriesReadUseCase seriesReadUseCase;
+    
+    @Operation(summary = "시리즈 상세 조회", description = "시리즈 ID를 이용해 시리즈를 상세 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공")
+    })
+    @GetMapping("/{seriesId}")
+    public SeriesDetailResponse getSeries(@PathVariable("seriesId") String seriesId) {
+        return SeriesDetailResponse.builder()
+                .seriesDetail(seriesReadUseCase.getSeries(seriesId))
+                .build();
+    }
     
     @Operation(summary = "시리즈 목록 조회", description = "블로그 ID를 이용해 시리즈 목록을 조회합니다.")
     @ApiResponses(value = {
@@ -29,8 +42,9 @@ public class SeriesQueryApi {
                     content = @Content(array = @ArraySchema(schema = @Schema(implementation = SeriesSummary.class))))
     })
     @GetMapping("/{blogId}")
-    public List<SeriesSummaryResponse> getSeries(@PathVariable("blogId") String blogId) {
-        // ...
-        return null;
+    public SeriesSummaryResponse getSeriesList(@PathVariable("blogId") String blogId) {
+        return SeriesSummaryResponse.builder()
+                .seriesSummary(seriesReadUseCase.getSeriesList(blogId))
+                .build();
     }
 }
