27b5713fcf5b7f1f1f2fa103bdf7a6b8c3689d1a
merge-simpson <letsdev@kakao.com>
2025-07-24 01:35:39 +0900

    feat(blog): add blog subscription command repository port interface
    define repository port for managing blog subscriptions with `transaction`, `save`, `deleteById`, `findByUserIdAndBlogId`, `countByUserId`, `countByBlogId`, and `existsByUserIdAndBlogId` methods

---
+++ services/blog/application/src/main/java/nettee/blolet/blog/application/port/BlogSubscriptionCommandRepositoryPort.java
@@ -0,0 +1,20 @@
+package nettee.blolet.blog.application.port;
+
+import nettee.blolet.blog.domain.BlogSubscription;
+
+import java.util.Optional;
+
+public interface BlogSubscriptionCommandRepositoryPort {
+    void transaction(Runnable runnable);
+
+    BlogSubscription save(BlogSubscription blog);
+
+    void deleteById(String id);
+
+    Optional<BlogSubscription> findByUserIdAndBlogId(String userId, String blogId);
+
+    int countByUserId(String userId);
+    int countByBlogId(String blogId);
+
+    boolean existsByUserIdAndBlogId(String userId, String blogId);
+}
