14f6f62654830efe6c59cb2a57363a60eefb8cf9
silberbullet <pkw961028@naver.com>
2025-07-24 00:39:22 +0900

    feat(series): add command api test code

---
+++ services/series/driving/web-mvc/src/test/java/nettee/series/driving/web/SeriesWebMvcTestApplication.java
@@ -0,0 +1,11 @@
+package nettee.series.driving.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesWebMvcTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeriesWebMvcTestApplication.class, args);
+    }
+}
