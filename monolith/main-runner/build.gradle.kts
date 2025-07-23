4c7d2f4d69da19e424b38382bf9ffb91851252a6
silberbullet <pkw961028@naver.com>
2025-07-24 00:15:54 +0900

    feat(monolith): add spring-boot-starter-validation module

--- monolith/main-runner/build.gradle.kts
+++ monolith/main-runner/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 
     // webmvc
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // db
     runtimeOnly("org.postgresql:postgresql:42.7.4")
