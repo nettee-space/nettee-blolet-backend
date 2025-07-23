c4297f50ca4c56bc9d9dbdd48828e9e3644ff361
merge-simpson <letsdev@kakao.com>
2025-07-23 10:50:46 +0900

    feat(blog): add findById method to BlogCommandRepositoryPort
    enable fetching blog by id returning Optional<Blog>

--- services/blog/application/src/main/java/nettee/blolet/blog/application/port/BlogCommandRepositoryPort.java
+++ services/blog/application/src/main/java/nettee/blolet/blog/application/port/BlogCommandRepositoryPort.java
@@ -2,6 +2,8 @@ package nettee.blolet.blog.application.port;
 
 import nettee.blolet.blog.domain.Blog;
 
+import java.util.Optional;
+
 public interface BlogCommandRepositoryPort {
     void transaction(Runnable runnable);
 
@@ -9,6 +11,7 @@ public interface BlogCommandRepositoryPort {
 
     void deleteById(String id);
 
+    Optional<Blog> findById(String id);
     int countByUserId(String userId);
     boolean existsById(String id);
 }
