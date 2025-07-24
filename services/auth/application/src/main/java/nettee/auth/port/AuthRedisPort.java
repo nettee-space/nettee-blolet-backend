2e609fc346c8b3f509207f9acbb281e8e29d6064
wch-os <wch1224.s@gmail.com>
2025-07-25 01:06:02 +0900

    refactor(auth): remove AuthRedisPort from signup

--- services/auth/application/src/main/java/nettee/auth/port/AuthRedisPort.java
+++
@@ -1,6 +0,0 @@
-package nettee.auth.port;
-
-public interface AuthRedisPort {
-    String generateRefreshToken(String loginId, String refreshTokenKey);
-    String generateAccessToken(String loginId);
-}
