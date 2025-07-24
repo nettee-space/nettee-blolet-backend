ae49818b43d1464a25017e58f11d7b1ab35e0b8c
silberbullet <pkw961028@naver.com>
2025-07-25 02:54:12 +0900

    feat(series): add series and series article command adapter

---
+++ services/series/driven/rdb/src/main/java/nettee/series/driven/rdb/persistence/SeriesCommandAdapter.java
@@ -0,0 +1,49 @@
+package nettee.series.driven.rdb.persistence;
+
+import lombok.RequiredArgsConstructor;
+import nettee.series.application.port.SeriesCommandRepositoryPort;
+import nettee.series.domain.Series;
+import nettee.series.driven.rdb.persistence.mapper.SeriesEntityMapper;
+import org.springframework.stereotype.Repository;
+
+import static nettee.series.exception.SeriesErrorCode.SERIES_NOT_FOUND;
+
+@Repository
+@RequiredArgsConstructor
+public class SeriesCommandAdapter implements SeriesCommandRepositoryPort {
+    
+    private final SeriesJpaRepository seriesJpaRepository;
+    private final SeriesEntityMapper seriesEntityMapper;
+    
+    @Override
+    public boolean existsByBlogIdAndTitle(String blogId, String title) {
+        return seriesJpaRepository.existsByBlogIdAndTitle(Long.valueOf(blogId), title);
+    }
+    
+    @Override
+    public Series save(Series series) {
+        var seriesEntity = seriesEntityMapper.toEntity(series);
+
+        return seriesEntityMapper.toDomain(seriesJpaRepository.save(seriesEntity));
+    }
+    
+    @Override
+    public Series update(Series series) {
+        var existsSeries = seriesJpaRepository.findById(Long.valueOf(series.getId()))
+                .orElseThrow(SERIES_NOT_FOUND::exception);
+        
+        existsSeries.prepareSeriesEntityUpdate()
+                .title(series.getTitle())
+                .description(series.getDescription())
+                .banner(seriesEntityMapper.base64ToBytes(series.getBanner()))
+                .displayOrder(series.getDisplayOrder())
+                .update();
+        
+        return seriesEntityMapper.toDomain(existsSeries);
+    }
+    
+    @Override
+    public void delete(String seriesId) {
+        seriesJpaRepository.deleteById(Long.valueOf(seriesId));
+    }
+}
