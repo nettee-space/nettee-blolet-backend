914b94802a7756621e40350fd06b4ed4205b4ccb
merge-simpson <letsdev@kakao.com>
2025-07-26 21:19:01 +0900

    refactor(blog): use `SubscriptionStats` from application layer instead of `SubscriptionCount` in webmvc

--- services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/dto/BlogCommandDto.java
+++ services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/dto/BlogCommandDto.java
@@ -2,6 +2,7 @@ package nettee.blolet.blog.web.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
+import nettee.blolet.blog.application.usecase.subscription.data.SubscriptionStats;
 import nettee.blolet.blog.readmodel.BlogReadModels.BlogDetail;
 
 public final class BlogCommandDto {
@@ -16,22 +17,14 @@ public final class BlogCommandDto {
     public record BlogUpdateResponse(BlogDetail blog) {}
 
     @Builder
-    public record BlogSubscribeResponse(SubscriptionCount counts) {}
+    public record BlogSubscribeResponse(SubscriptionStats counts) {}
 
     @Builder
-    public record BlogUnsubscribeResponse(SubscriptionCount counts) {}
+    public record BlogUnsubscribeResponse(SubscriptionStats counts) {}
 
     @Builder
-    public record BlogNewsletterSubscribeResponse(SubscriptionCount counts) {}
+    public record BlogNewsletterSubscribeResponse(SubscriptionStats counts) {}
 
     @Builder
-    public record BlogNewsletterUnsubscribeResponse(SubscriptionCount counts) {}
-
-    @Builder
-    public record SubscriptionCount(
-            @Schema(description = "이 블로그를 구독한 사용자 수", example = "999")
-            Integer totalSubscribers,
-            @Schema(description = "사용자가 구독한 블로그 수", example = "99")
-            Integer userSubscriptions
-    ) {}
+    public record BlogNewsletterUnsubscribeResponse(SubscriptionStats counts) {}
 }
