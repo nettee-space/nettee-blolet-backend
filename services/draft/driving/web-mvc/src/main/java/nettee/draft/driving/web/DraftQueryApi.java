080179ab011425da9c40cb0268fef4e714a2936c
sweetykr7 <sweetykr@naver.com>
2025-07-28 14:43:57 +0900

    refactor(draft): remove non required import line

--- services/draft/driving/web-mvc/src/main/java/nettee/draft/driving/web/DraftQueryApi.java
+++ services/draft/driving/web-mvc/src/main/java/nettee/draft/driving/web/DraftQueryApi.java
@@ -11,7 +11,6 @@ import nettee.draft.readmodel.DraftReadModels.DraftSummary;
 import nettee.draft.application.usecase.DraftReadByStatusesUseCase;
 import nettee.draft.application.usecase.DraftReadUseCase;
 import nettee.draft.domain.type.DraftStatus;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
