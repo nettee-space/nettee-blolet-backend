3f41183293522fd6482e210cbe75cb49b52ec95a
전상은 <wowddok99@gmail.com>
2025-07-30 01:00:52 +0900

    refactor(jpa): revert to previous state by removing unnecessary annotations
    restores original logic before annotation changes

--- core/jpa-core/src/main/java/nettee/jpa/support/LongBaseTimeEntity.java
+++ core/jpa-core/src/main/java/nettee/jpa/support/LongBaseTimeEntity.java
@@ -3,16 +3,12 @@ package nettee.jpa.support;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
-@SuperBuilder
-@NoArgsConstructor
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
