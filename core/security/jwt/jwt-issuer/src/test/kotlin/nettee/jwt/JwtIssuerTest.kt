754c19db75d0c5841ceaad83b02282c806ee291b
wch-os <wch1224.s@gmail.com>
2025-07-29 22:34:31 +0900

    test(jwt): verify correct algorithm selection for long HMAC/RSA keys

--- core/security/jwt/jwt-issuer/src/test/kotlin/nettee/jwt/JwtIssuerTest.kt
+++ core/security/jwt/jwt-issuer/src/test/kotlin/nettee/jwt/JwtIssuerTest.kt
@@ -1,48 +1,87 @@
 package nettee.jwt
 
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldNotBeBlank
 import java.security.KeyPairGenerator
+import java.security.SecureRandom
 import java.util.*
 
 class JwtIssuerTest : FreeSpec({
 
-    "HMAC 알고리즘을 사용한 액세스 토큰을 생성할 수 있다" {
-        // HMAC 비밀 키 생성
-        val keyType = "HMAC"
-        val secretKey = Base64.getEncoder()
-            .encodeToString("nettee-blolet-jwt-secret-key-extend".toByteArray())
+    "512bit HMAC 키로 jwt를 생성하고, HS512 알고리즘이 자동 추론되어 적용되었는지 검증한다" {
+        // given
+        val secureRandom = SecureRandom()
+        val keyBytes = ByteArray(64) // 64byte(=512bit) 배열 생성
+        secureRandom.nextBytes(keyBytes)  // keyBytes 배열을 랜덤 바이트로 채움
+        val secretKey = Base64.getEncoder().encodeToString(keyBytes)
 
         val issuer = JwtIssuer(
-            keyType,
+            "HMAC",
             secretKey,
             null,
             60L
         )
 
+        // when
         val token = issuer.issueAccessToken("sun", mapOf("role" to "USER"))
-        println("HMAC Access Token = $token")
         token.shouldNotBeBlank()
+
+        // then
+        // 1. 검증 키 생성
+        val verificationKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secretKey))
+
+        // 2. jwt 파싱
+        val parsedClaims = Jwts.parser()
+            .verifyWith(verificationKey)
+            .build()
+            .parseSignedClaims(token)
+
+        // 3. subject, role 검증
+        parsedClaims.payload.subject shouldBe "sun"
+        parsedClaims.payload["role"] shouldBe "USER"
+
+        // 4. HMAC 512bit 키에 대해 RS512 알고리즘으로 자동 추론하여 적용하는지 검증
+        val algorithm = parsedClaims.header.algorithm
+        algorithm shouldBe "HS512"
     }
 
-
-    "RSA 알고리즘을 사용한 액세스 토큰을 생성할 수 있다" {
+    "4096bit RSA 키로 jwt를 생성하고, RS512 알고리즘이 자동 추론되어 적용되었는지 검증한다" {
+        // given
         val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
-        keyPairGenerator.initialize(2048)
-        val keyPair = keyPairGenerator.generateKeyPair()
-
+        keyPairGenerator.initialize(4096)           // 4096bit 키 생성
+        val keyPair = keyPairGenerator.generateKeyPair()    // 공개키, 개인키 쌍 생성
         val privateKey = Base64.getEncoder().encodeToString(keyPair.private.encoded)
-        val keyType = "RSA"
 
         val issuer = JwtIssuer(
-            keyType,
+            "RSA",
             null,
             privateKey,
             60L
         )
 
+        // when
         val token = issuer.issueAccessToken("sun", mapOf("role" to "USER"))
-        println("RSA Access Token = $token")
         token.shouldNotBeBlank()
+
+        // then
+        // 1. 검증에는 공개키 사용
+        val verificationKey = keyPair.public
+
+        // 2. jwt 파싱
+        val parsedClaims = Jwts.parser()
+            .verifyWith(verificationKey)
+            .build()
+            .parseSignedClaims(token)
+
+        // 3. subject, role 검증
+        parsedClaims.payload.subject shouldBe "sun"
+        parsedClaims.payload["role"] shouldBe "USER"
+
+        // 4. RSA 512bit 키에 대해 RS512 알고리즘으로 자동 추론하여 적용하는지 검증
+        val algorithm = parsedClaims.header.algorithm
+        algorithm shouldBe "RS512"
     }
 })
