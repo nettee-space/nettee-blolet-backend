f6b4dfb1428e20a551a94b51092e6f8856e2a69d
merge-simpson <letsdev@kakao.com>
2025-07-31 00:35:19 +0900

    refactor(blog): rename `unsubscribeBlog` parameter `username` to `userId` in service

--- services/blog/application/src/main/java/nettee/blolet/blog/application/service/BlogSubscriptionCommandService.java
+++ services/blog/application/src/main/java/nettee/blolet/blog/application/service/BlogSubscriptionCommandService.java
@@ -44,8 +44,8 @@ public class BlogSubscriptionCommandService implements BlogSubscriptionUseCase, 
     }
 
     @Override
-    public void unsubscribeBlog(String username, String blogId) {
-        BlogSubscription subscription = commandRepository.findByUserIdAndBlogId(username, blogId)
+    public void unsubscribeBlog(String userId, String blogId) {
+        BlogSubscription subscription = commandRepository.findByUserIdAndBlogId(userId, blogId)
                 .orElseThrow(UNSUBSCRIBED_BLOG::exception);
         commandRepository.deleteById(subscription.getId());
     }
