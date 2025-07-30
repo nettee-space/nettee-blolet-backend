01f5a2122c59dfdabe822769e15a74a7e7e17627
merge-simpson <letsdev@kakao.com>
2025-07-31 01:39:52 +0900

    refactor(blog): rename `unsubscribeBlog` parameter `username` to `userId` in use case

--- services/blog/application/src/main/java/nettee/blolet/blog/application/usecase/subscription/BlogUnsubscriptionUseCase.java
+++ services/blog/application/src/main/java/nettee/blolet/blog/application/usecase/subscription/BlogUnsubscriptionUseCase.java
@@ -3,5 +3,5 @@ package nettee.blolet.blog.application.usecase.subscription;
 import nettee.blolet.blog.application.usecase.subscription.data.SubscriptionStats;
 
 public interface BlogUnsubscriptionUseCase {
-    SubscriptionStats unsubscribeBlog(String username, String blogId);
+    SubscriptionStats unsubscribeBlog(String userId, String blogId);
 }
