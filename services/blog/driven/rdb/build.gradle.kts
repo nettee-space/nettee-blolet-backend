cd5198c0a6d6241bf5b24b3226b8311b4be2da63
merge-simpson <letsdev@kakao.com>
2025-07-31 04:32:25 +0900

    build(blog): add mapstruct dependencies to rdb adapter
    Added MapStruct 1.6.3 implementation and annotation processors (mapstruct-processor and lombok-mapstruct-binding) in blog rdb adapter’s build.gradle.kts.

--- services/blog/driven/rdb/build.gradle.kts
+++ services/blog/driven/rdb/build.gradle.kts
@@ -5,4 +5,9 @@ dependencies {
     api(project(blogApi))
     api(project(blogApplication))
     api(project(":jpa-core"))
+
+    // mapstruct
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 }
