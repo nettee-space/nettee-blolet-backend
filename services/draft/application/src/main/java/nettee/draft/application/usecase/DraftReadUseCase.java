05ed06b83da9942a17d5ab4f1d8c7d0f6f08f73f
sweetykr7 <sweetykr@naver.com>
2025-08-01 11:29:42 +0900

    feat(draft): add getDraftTitlesByIds usecase

--- services/draft/application/src/main/java/nettee/draft/application/usecase/DraftReadUseCase.java
+++ services/draft/application/src/main/java/nettee/draft/application/usecase/DraftReadUseCase.java
@@ -1,11 +1,12 @@
 package nettee.draft.application.usecase;
 
 import nettee.draft.readmodel.DraftReadModels.DraftDetail;
-import nettee.draft.readmodel.DraftReadModels.DraftSummary;
-import org.springframework.data.domain.Page;
+import nettee.draft.readmodel.DraftReadModels.DraftTitle;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DraftReadUseCase {
     Optional<DraftDetail> getDraft(String id);
+    List<DraftTitle> getDraftTitlesByIds(List<String> ids);
 }
