383ef8d84abb2cd9ffa0efec62c29fdaa6f23c1f
sweetykr7 <sweetykr@naver.com>
2025-08-01 09:51:43 +0900

    refactor(draft): change drivinig mapping patch to put

--- services/draft/driving/web-mvc/src/main/java/nettee/draft/draftblock/driving/web/DraftBlockCommandApi.java
+++ services/draft/driving/web-mvc/src/main/java/nettee/draft/draftblock/driving/web/DraftBlockCommandApi.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -59,7 +60,7 @@ public class DraftBlockCommandApi {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공")
     })
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public DraftBlockCommandResponse updateDraftBlock(
             @PathVariable("id") String id,
--- services/draft/driving/web-mvc/src/main/java/nettee/draft/driving/web/DraftCommandApi.java
+++ services/draft/driving/web-mvc/src/main/java/nettee/draft/driving/web/DraftCommandApi.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -57,7 +58,7 @@ public class DraftCommandApi {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공")
     })
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public DraftCommandResponse updateDraft(
             @PathVariable("id") String id,
