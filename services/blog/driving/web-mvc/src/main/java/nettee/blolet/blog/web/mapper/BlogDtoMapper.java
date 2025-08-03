ac02a1613f030bfc03105a34515758cefe15552e
merge-simpson <letsdev@kakao.com>
2025-08-04 04:07:25 +0900

    fix(blog): map `id` from `blogId` in `BlogDtoMapper`
    added `@Mapping(target="id", source="blogId")` to ensure the domain object's id is correctly set from the `blogId` parameter in `BlogDtoMapper.toDomain`

--- services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/mapper/BlogDtoMapper.java
+++ services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/mapper/BlogDtoMapper.java
@@ -4,9 +4,11 @@ import nettee.blolet.blog.domain.Blog;
 import nettee.blolet.blog.web.dto.BlogCommandDto.BlogUpdateCommand;
 import nettee.blolet.blog.web.dto.BlogCommandDto.BlogUpdateResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BlogDtoMapper {
+    @Mapping(target = "id", source = "blogId")
     Blog toDomain(String blogId, BlogUpdateCommand dto);
     BlogUpdateResponse toResponse(Blog blog);
 }
