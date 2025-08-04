10bf7a85c60bf56668ff73b53c2250a03cdc6878
merge-simpson <letsdev@kakao.com>
2025-08-05 00:37:47 +0900

    feat(blog): implement use case to verify blog ownership in service

---
+++ services/blog/application/src/main/java/nettee/blolet/blog/application/service/BlogQueryService.java
@@ -0,0 +1,46 @@
+package nettee.blolet.blog.application.service;
+
+import lombok.RequiredArgsConstructor;
+import nettee.blolet.blog.application.port.BlogQueryRepositoryPort;
+import nettee.blolet.blog.application.usecase.BlogReadUseCase;
+import nettee.blolet.blog.application.usecase.BlogOwnershipVerifyUseCase;
+import nettee.blolet.blog.domain.Blog;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import static nettee.blolet.blog.exception.BlogErrorCode.BLOG_NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
+public class BlogQueryService implements BlogReadUseCase, BlogOwnershipVerifyUseCase {
+
+    private final BlogQueryRepositoryPort repository;
+
+    @Override
+    public Blog findById(String id) {
+        return repository.findById(id).orElseThrow(BLOG_NOT_FOUND::exception);
+    }
+
+    @Override
+    public Map<String, Blog> findByUserProfileIds(Set<String> profileIds) {
+        return repository.findAllByProfileIdIn(profileIds);
+    }
+
+    @Override
+    public boolean verifyOwnershipByUserId(String userId, String blogId) {
+        var blog = repository.findById(blogId)
+                .orElseThrow(BLOG_NOT_FOUND::exception);
+        return Objects.equals(userId, blog.getUserId());
+    }
+
+    @Override
+    public boolean verifyOwnershipByProfileId(String profileId, String blogId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+//        var blog = repository.findById(blogId)
+//                .orElseThrow(BLOG_NOT_FOUND::exception);
+//        return Objects.equals(profileId,blog.getProfileId());
+    }
+}
