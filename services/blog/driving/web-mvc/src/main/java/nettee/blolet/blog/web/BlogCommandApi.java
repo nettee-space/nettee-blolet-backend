26175b3e5a62d4bf7f876fdb5b85b6cb1d86dadf
merge-simpson <letsdev@kakao.com>
2025-08-06 01:52:27 +0900

    fix(blog): throw 501 not implemented exception for blog & newsletter subscriptions
    replace `new UnsupportedOperationException("...")` with `BLOG_API_NOT_IMPLEMENTED.exception()` in blog subscription/unsubscription endpoints and newsletter subscription/unsubscription endpoints

--- services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/BlogCommandApi.java
+++ services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/BlogCommandApi.java
@@ -22,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import static nettee.blolet.blog.exception.BlogErrorCode.BLOG_API_NOT_IMPLEMENTED;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("blogs")
@@ -58,7 +60,7 @@ public class BlogCommandApi {
             description = "블로그를 구독합니다."
     )
     public BlogSubscribeResponse subscribeBlog(@PathVariable("blogId") String blogId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw BLOG_API_NOT_IMPLEMENTED.exception();
     }
 
     @DeleteMapping("/{blogId}/subscribe")
@@ -67,7 +69,7 @@ public class BlogCommandApi {
             description = "구독한 블로그의 구독을 취소합니다. (TODO 정책 논의: 아마도 뉴스레터 구독도 함께 취소될 것입니다.)"
     )
     public BlogUnsubscribeResponse unsubscribeBlog(@PathVariable("blogId") String blogId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw BLOG_API_NOT_IMPLEMENTED.exception();
     }
 
     @PostMapping("/{blogId}/newsletter")
@@ -76,7 +78,7 @@ public class BlogCommandApi {
             description = "블로그의 뉴스레터 수신을 동의합니다. (TODO 정책 논의: 아직 구독하지 않은 블로그라면 아마도 구독도 함께 될 것입니다.)"
     )
     public BlogNewsletterSubscribeResponse subscribeNewsletter(@PathVariable("blogId") String blogId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw BLOG_API_NOT_IMPLEMENTED.exception();
     }
 
     @DeleteMapping("/{blogId}/newsletter")
@@ -85,6 +87,6 @@ public class BlogCommandApi {
             description = "블로그의 뉴스레터 수신 동의를 철회합니다."
     )
     public BlogNewsletterUnsubscribeResponse unsubscribeNewsletter(@PathVariable("blogId") String blogId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw BLOG_API_NOT_IMPLEMENTED.exception();
     }
 }
