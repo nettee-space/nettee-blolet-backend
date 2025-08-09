fc26899d72b1ef3784a80ceeaded71f70765f9c1
merge-simpson <letsdev@kakao.com>
2025-08-10 08:01:22 +0900

    feat(blog): add `toDetail` in dto mapper

--- services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/mapper/BlogDtoMapper.java
+++ services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/mapper/BlogDtoMapper.java
@@ -1,6 +1,7 @@
 package nettee.blolet.blog.web.mapper;
 
 import nettee.blolet.blog.domain.Blog;
+import nettee.blolet.blog.readmodel.BlogReadModels.BlogDetail;
 import nettee.blolet.blog.web.dto.BlogCommandDto.BlogUpdateCommand;
 import nettee.blolet.blog.web.dto.BlogCommandDto.BlogUpdateResponse;
 import org.mapstruct.Mapper;
@@ -11,4 +12,5 @@ public interface BlogDtoMapper {
     @Mapping(target = "id", source = "blogId")
     Blog toDomain(String blogId, BlogUpdateCommand dto);
     BlogUpdateResponse toResponse(Blog blog);
+    BlogDetail toDetail(Blog blog);
 }
