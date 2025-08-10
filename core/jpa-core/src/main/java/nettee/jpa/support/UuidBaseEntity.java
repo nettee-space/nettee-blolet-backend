70725fbdfab4af602ef40df2d6ae37fcb4977dda
전상은 <wowddok99@gmail.com>
2025-08-10 23:55:06 +0900

    fix(jpa): enable superclass field initialization via `@SuperBuilder` in all base entities
    Applied `@SuperBuilder` and `@NoArgsConstructor` to multiple base entity classes to allow superclass field population in JPA entities while maintaining default constructor requirement.

--- core/jpa-core/src/main/java/nettee/jpa/support/UuidBaseEntity.java
+++ core/jpa-core/src/main/java/nettee/jpa/support/UuidBaseEntity.java
@@ -5,10 +5,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 import java.util.UUID;
 
+@SuperBuilder
+@NoArgsConstructor
 @Getter
 @MappedSuperclass
 public abstract class UuidBaseEntity implements Serializable {
