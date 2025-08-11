de8edbe2a6c65f7ea5d99654c0ad84ab94513ca5
전상은 <wowddok99@gmail.com>
2025-08-12 00:56:59 +0900

    chore(article): remove unused import

--- services/article/driven/rdb/src/main/java/nettee/article/driven/rdb/persistence/mapper/ArticleEntityMapper.java
+++ services/article/driven/rdb/src/main/java/nettee/article/driven/rdb/persistence/mapper/ArticleEntityMapper.java
@@ -3,10 +3,10 @@ package nettee.article.driven.rdb.persistence.mapper;
 import nettee.article.domain.Article;
 import nettee.article.driven.rdb.entity.ArticleEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ArticleEntityMapper {
+
     Article toDomain(ArticleEntity articleEntity);
     ArticleEntity toEntity(Article article);
 }
