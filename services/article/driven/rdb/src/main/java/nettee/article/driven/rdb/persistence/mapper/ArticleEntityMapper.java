7810ecdf799c47eaf8cb5b42ad64304237eb438f
전상은 <wowddok99@gmail.com>
2025-08-12 20:07:17 +0900

    feat(article): convert projection to query model using mapper in ArticleQueryAdapter

--- services/article/driven/rdb/src/main/java/nettee/article/driven/rdb/persistence/mapper/ArticleEntityMapper.java
+++ services/article/driven/rdb/src/main/java/nettee/article/driven/rdb/persistence/mapper/ArticleEntityMapper.java
@@ -2,6 +2,10 @@ package nettee.article.driven.rdb.persistence.mapper;
 
 import nettee.article.domain.Article;
 import nettee.article.driven.rdb.entity.ArticleEntity;
+import nettee.article.driven.rdb.projection.ArticleQueryProjections.ArticleDetailProjection;
+import nettee.article.driven.rdb.projection.ArticleQueryProjections.ArticleSummaryProjection;
+import nettee.article.readmodel.ArticleQueryModels.ArticleDetail;
+import nettee.article.readmodel.ArticleQueryModels.ArticleSummary;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -9,4 +13,6 @@ public interface ArticleEntityMapper {
 
     Article toDomain(ArticleEntity articleEntity);
     ArticleEntity toEntity(Article article);
+    ArticleDetail toQueryModel(ArticleDetailProjection articleDetailProjection);
+    ArticleSummary toQueryModel(ArticleSummaryProjection articleSummaryProjection);
 }
