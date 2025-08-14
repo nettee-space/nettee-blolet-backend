515bc7e8103e542a467d90f2177fd11a9452d5cc
merge-simpson <letsdev@kakao.com>
2025-08-15 04:56:52 +0900

    chore(flyway): define namespaced session variables for dummy ids and credentials in `R__init_dummies.sql` for local profile
    Use the custom GUC namespace `dummy.*` so other migration scripts can read the same session-scoped values via `current_setting('dummy.user_id')::bigint`, `current_setting('dummy.profile_id')::bigint`, and `current_setting('dummy.user_encoded_password')`. Includes `USER_ID`, `PROFILE_ID`, `BLOG_ID`, `USERNAME`, `USER_RAW_PASSWORD`, and `USER_ENCODED_PASSWORD` (argon2id).

---
+++ monolith/main-runner/src/main/resources/db/migration/local/R__init_dummies.sql
@@ -0,0 +1,9 @@
+-- 네임스페이스 사용 필수 (ex: dummy.~)
+SET dummy.USER_ID               = '33333333310000000';
+SET dummy.PROFILE_ID            = '33333333320000000';
+SET dummy.BLOG_ID               = '33333333330000000';
+SET dummy.USERNAME              = 'sun';
+SET dummy.USER_RAW_PASSWORD     = 'Blolet1225!';
+SET dummy.USER_ENCODED_PASSWORD =
+    '$argon2id$v=19$m=32768,t=4,p=1$y2CjGj9VT7FUmmRu1brflw$h9MfPg2iT2Doh9w/J1ORBpmOE0/ZWVa2q4kMIPlmqc8';
+-- current_setting('dummy.USERID')::bigint
