ad4e610383e8d1ecc9a837852e4d7fa8245b3e90
merge-simpson <letsdev@kakao.com>
2025-08-15 04:58:27 +0900

    chore(flyway): insert dummy user using predefined session variables in `R__init_dummies.sql` for local profile
    Populate `auth.user` table with a dummy account referencing `dummy.USER_ID`, `dummy.USERNAME`, and `dummy.USER_ENCODED_PASSWORD` set earlier. This ensures consistent IDs and credentials across migration scripts.

--- monolith/main-runner/src/main/resources/db/migration/local/R__init_dummies.sql
+++ monolith/main-runner/src/main/resources/db/migration/local/R__init_dummies.sql
@@ -7,3 +7,20 @@ SET dummy.USER_RAW_PASSWORD     = 'Blolet1225!';
 SET dummy.USER_ENCODED_PASSWORD =
     '$argon2id$v=19$m=32768,t=4,p=1$y2CjGj9VT7FUmmRu1brflw$h9MfPg2iT2Doh9w/J1ORBpmOE0/ZWVa2q4kMIPlmqc8';
 -- current_setting('dummy.USERID')::bigint
+
+INSERT INTO "auth"."user" (
+    "id",
+    "login_id",
+    "username",
+    "encoded_password",
+    "nickname",
+    "email"
+) VALUES (
+    current_setting('dummy.USER_ID')::bigint,
+    'sun123',
+    current_setting('dummy.USERNAME')::varchar,
+    -- raw password: Blolet1225!
+    current_setting('dummy.USER_ENCODED_PASSWORD')::varchar,
+    '동굴',
+    'sun@gmail.com'
+);
