265f60d3183e58c4c9e2b73192968a4ff3e171e9
merge-simpson <letsdev@kakao.com>
2025-08-15 19:13:16 +0900

    build(blog): scan `blog-client` module at `blog.settings.gradle.kts`

--- services/blog/blog.settings.gradle.kts
+++ services/blog/blog.settings.gradle.kts
@@ -6,6 +6,7 @@ val blogReadModel: String by settings
 val blogApplication: String by settings
 val blogRdbAdapter: String by settings
 val blogWebMvcAdapter: String by settings
+val blogClient: String by settings
 
 fun getDirectories(vararg names: String): (String) -> File {
     var dir = rootDir
@@ -32,6 +33,7 @@ include(
     blogApplication,
     blogRdbAdapter,
     blogWebMvcAdapter,
+    blogClient,
 )
 
 project(blog).projectDir = blogDirectory("blog")
@@ -42,3 +44,4 @@ project(blogReadModel).projectDir = blogDirectory("readmodel")
 project(blogApplication).projectDir = blogDirectory("application")
 project(blogRdbAdapter).projectDir = blogDirectory("rdb")
 project(blogWebMvcAdapter).projectDir = blogDirectory("web-mvc")
+project(blogClient).projectDir = blogDirectory("client")
