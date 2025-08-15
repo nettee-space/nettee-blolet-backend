6522c89e18c53ca74acf5026c0d0d2a5de65a813
merge-simpson <letsdev@kakao.com>
2025-08-15 19:12:25 +0900

    build(blog): add caffeine cache dependency in blog client

--- services/blog/export/client/build.gradle.kts
+++ services/blog/export/client/build.gradle.kts
@@ -3,4 +3,7 @@ val blogApi: String by project
 dependencies {
     api(project(blogApi))
     implementation(project(":rest-client"))
+
+    // cache
+    implementation("com.github.ben-manes.caffeine:caffeine:3.2.2")
 }
