af5a04f0e9163f47b2ebb4e1723840d8fd520856
merge-simpson <letsdev@kakao.com>
2025-08-16 07:00:10 +0900

    chore(draft): remove unnecessary annotation in dto mapper

--- services/draft/driving/web-mvc/src/main/java/nettee/draft/driving/web/mapper/DraftDtoMapper.java
+++ services/draft/driving/web-mvc/src/main/java/nettee/draft/driving/web/mapper/DraftDtoMapper.java
@@ -2,18 +2,16 @@ package nettee.draft.driving.web.mapper;
 
 import nettee.draft.domain.Draft;
 import nettee.draft.domain.type.DraftStatus;
-import nettee.draft.readmodel.DraftReadModels.DraftDetail;
 import nettee.draft.driving.web.dto.DraftCommandDto.DraftCreateCommand;
 import nettee.draft.driving.web.dto.DraftCommandDto.DraftUpdateCommand;
 import nettee.draft.driving.web.dto.DraftQueryDto.DraftDetailResponse;
+import nettee.draft.readmodel.DraftReadModels.DraftDetail;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.Optional;
 
 @Mapper(componentModel = "spring")
 public interface DraftDtoMapper {
-    @Mapping(target = "id", source = "userId")
     Draft toDomain(DraftCreateCommand command, DraftStatus status);
     Draft toDomain(String id, DraftUpdateCommand command);
     DraftDetailResponse toDtoDetail(Optional<DraftDetail> board);
