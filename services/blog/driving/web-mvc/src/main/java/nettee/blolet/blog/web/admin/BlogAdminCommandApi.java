abbf5d495074596c6170a9ceafbb7a7f8d66b5c8
merge-simpson <letsdev@kakao.com>
2025-08-17 17:59:38 +0900

    feat(blog): append internal api to create blog and fix spec of existing admin create api endpoint
    - Use `BlogValidator` instead of `Preconditions`
    - Fill missing fields of admin blog create request DTO

--- services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/admin/BlogAdminCommandApi.java
+++ services/blog/driving/web-mvc/src/main/java/nettee/blolet/blog/web/admin/BlogAdminCommandApi.java
@@ -5,31 +5,41 @@ import lombok.RequiredArgsConstructor;
 import nettee.blolet.blog.application.usecase.BlogCreateUseCase;
 import nettee.blolet.blog.web.admin.dto.BlogAdminCommandDto.BlogCreateCommand;
 import nettee.blolet.blog.web.admin.dto.BlogAdminCommandDto.BlogCreateResponse;
+import nettee.blolet.blog.web.admin.dto.BlogAdminCommandDto.BlogInternalCreateCommand;
 import nettee.blolet.blog.web.admin.mapper.BlogAdminDtoMapper;
 import nettee.jwt.annotation.AuthUser;
 import nettee.jwt.annotation.AuthorizedUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("admin/blogs")
 public class BlogAdminCommandApi {
 
     private final BlogCreateUseCase createUseCase;
     private final BlogAdminDtoMapper mapper;
 
-    @PostMapping
+    @PostMapping("/admin/blogs")
     @ResponseStatus(HttpStatus.CREATED)
-    public BlogCreateResponse create(
+    public BlogCreateResponse createByAdmin(
             @RequestBody @Valid BlogCreateCommand requestBody,
             @AuthUser AuthorizedUser user
     ) {
         var blog = mapper.toDomain(user.userId(), requestBody);
+        return mapper.toResponse(
+                createUseCase.save(blog)
+        );
+    }
+
+    @PostMapping("/internal/blogs")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BlogCreateResponse create(
+            @RequestBody @Valid BlogInternalCreateCommand requestBody
+    ) {
+        var blog = mapper.toDomain(requestBody);
         return mapper.toResponse(
                 createUseCase.save(blog)
         );
