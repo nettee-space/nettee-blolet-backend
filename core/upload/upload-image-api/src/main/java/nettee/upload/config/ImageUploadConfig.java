a99ac03937c7c07812c24d4257a6df880230b0b4
전상은 <wowddok99@gmail.com>
2025-08-19 16:58:18 +0900

    refactor(image): remove `@EnableConfigurationProperties` from `ImageUploadConfig`

--- core/upload/upload-image-api/src/main/java/nettee/upload/config/ImageUploadConfig.java
+++ core/upload/upload-image-api/src/main/java/nettee/upload/config/ImageUploadConfig.java
@@ -1,10 +1,7 @@
 package nettee.upload.config;
 
-import nettee.upload.properties.ImageUploadProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(ImageUploadProperties.class)
 public class ImageUploadConfig {
 }
