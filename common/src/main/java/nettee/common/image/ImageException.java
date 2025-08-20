21eb5e31fc3dbe235cf10ff81f59cf83de9b3376
전상은 <wowddok99@gmail.com>
2025-08-20 17:12:27 +0900

    feat(common): add `ImageFormatDetector`

---
+++ common/src/main/java/nettee/common/image/ImageException.java
@@ -0,0 +1,34 @@
+package nettee.common.image;
+
+import nettee.common.CustomException;
+import nettee.common.ErrorCode;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class ImageException extends CustomException {
+
+    public ImageException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public ImageException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode, cause);
+    }
+
+    public ImageException(ErrorCode errorCode, Runnable runnable) {
+        super(errorCode, runnable);
+    }
+
+    public ImageException(ErrorCode errorCode, Runnable runnable, Throwable cause) {
+        super(errorCode, runnable, cause);
+    }
+
+    public ImageException(ErrorCode errorCode, Supplier<Map<String, Object>> payloadSupplier) {
+        super(errorCode, payloadSupplier);
+    }
+
+    public ImageException(ErrorCode errorCode, Supplier<Map<String, Object>> payloadSupplier, Throwable cause) {
+        super(errorCode, payloadSupplier, cause);
+    }
+}
