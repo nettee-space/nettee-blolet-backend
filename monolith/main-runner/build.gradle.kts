ba0714fd601a7f8c4841e436b165ffa008a0a153
merge-simpson <letsdev@kakao.com>
2025-08-20 13:30:01 +0900

    build(monolith): include `spring-boot-starter-actuator` in `build.gradle.kts`

--- monolith/main-runner/build.gradle.kts
+++ monolith/main-runner/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     // webmvc
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     // db
     runtimeOnly("org.postgresql:postgresql:42.7.4")
