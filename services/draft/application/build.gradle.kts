a6167acdbcf786b8a659d5a874acb3382716f648
merge-simpson <letsdev@kakao.com>
2025-08-24 11:27:38 +0900

    refactor(draft): replace direct dependencies with draft api module in draft application
    Now `draft-application` can utilize features of `draft-api` module while still exposing simplified dependencies.
    This change ensures consistency and avoids redundant declarations of domain, exception, and read-model modules.
    
    이번 변경으로 `draft-application`에서 개별 모듈(`domain`, `exception`, `read-model`)을 직접 의존하지 않고, `draft-api` 모듈 하나를 통해 일괄적으로 기능을 가져올 수 있습니다. 이를 통해 의존성 관리를 단순화하고, API 모듈 고유의 기능도 활용할 수 있습니다.

--- services/draft/application/build.gradle.kts
+++ services/draft/application/build.gradle.kts
@@ -1,13 +1,8 @@
-val draftDomain: String by project
-val draftException: String by project
-val draftReadModel: String by project
-
+val draftApi: String by project
 val blogClientWebMvc: String by project
 
 dependencies {
-    api(project(draftDomain))
-    api(project(draftException))
-    api(project(draftReadModel))
+    api(project(draftApi))
 
     // internal clients
     api(project(blogClientWebMvc))
