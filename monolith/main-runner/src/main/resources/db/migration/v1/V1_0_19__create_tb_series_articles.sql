4774d134f30fc9b398b45448cbe0f4e4a2bf354e
merge-simpson <letsdev@kakao.com>
2025-08-30 23:56:32 +0900

    chore(monolith): fix schema "series" to "article" and replace "banner" with "banner_url" in "series" table

--- monolith/main-runner/src/main/resources/db/migration/v1/V1_0_19__create_tb_series_articles.sql
+++ monolith/main-runner/src/main/resources/db/migration/v1/V1_0_19__create_tb_series_articles.sql
@@ -1,8 +1,4 @@
--- 스키마가 없다면 먼저 생성
-CREATE SCHEMA IF NOT EXISTS "series";
-
--- Schema: series
-CREATE TABLE IF NOT EXISTS "series"."series_article" (
+CREATE TABLE IF NOT EXISTS "article"."series_article" (
     "id"                BIGSERIAL,
     "series_id"         BIGINT,
     "article_id"        BIGINT, -- 비정규화
@@ -13,19 +9,23 @@ CREATE TABLE IF NOT EXISTS "series"."series_article" (
 );
 
 --테이블 코멘트
-COMMENT ON TABLE "series"."series_article" IS '시리즈아티클';
+COMMENT ON TABLE "article"."series_article" IS '시리즈아티클';
 
 -- 컬럼 코멘트
-COMMENT ON COLUMN "series"."series_article"."id"                IS '시리즈아티클 PK';
-COMMENT ON COLUMN "series"."series_article"."series_id"         IS '시리즈 PK';
-COMMENT ON COLUMN "series"."series_article"."article_id"        IS '아티클 PK';
-COMMENT ON COLUMN "series"."series_article"."draft_id"          IS '임시아티클 PK';
-COMMENT ON COLUMN "series"."series_article"."display_order"     IS '사용자정렬순서';
-COMMENT ON COLUMN "series"."series_article"."created_at"        IS '생성시간';
-COMMENT ON COLUMN "series"."series_article"."updated_at"        IS '마지막 수정시간';
+COMMENT ON COLUMN "article"."series_article"."id"                IS '시리즈아티클 PK';
+COMMENT ON COLUMN "article"."series_article"."series_id"         IS '시리즈 PK';
+COMMENT ON COLUMN "article"."series_article"."article_id"        IS '아티클 PK';
+COMMENT ON COLUMN "article"."series_article"."draft_id"          IS '임시아티클 PK';
+COMMENT ON COLUMN "article"."series_article"."display_order"     IS '사용자정렬순서';
+COMMENT ON COLUMN "article"."series_article"."created_at"        IS '생성시간';
+COMMENT ON COLUMN "article"."series_article"."updated_at"        IS '마지막 수정시간';
 
-ALTER TABLE "series"."series_article" ADD CONSTRAINT "pk_series_article" PRIMARY KEY ("id");
-ALTER TABLE "series"."series_article" ADD CONSTRAINT "uq_series_article_series_id_article_id" UNIQUE ("series_id", "article_id");
-ALTER TABLE "series"."series_article" ADD CONSTRAINT "uq_series_article_series_id_draft_id" UNIQUE ("series_id", "draft_id");
+ALTER TABLE "article"."series_article" ADD CONSTRAINT "pk_series_article" PRIMARY KEY ("id");
+ALTER TABLE "article"."series_article"
+    ADD CONSTRAINT "uq_series_article_series_id_article_id"
+    UNIQUE ("series_id", "article_id");
+ALTER TABLE "article"."series_article"
+    ADD CONSTRAINT "uq_series_article_series_id_draft_id"
+    UNIQUE ("series_id", "draft_id");
 
-CREATE INDEX IF NOT EXISTS idx_series_article_series_id ON series.series_article (series_id);
+CREATE INDEX IF NOT EXISTS idx_series_article_series_id ON article.series_article (series_id);
