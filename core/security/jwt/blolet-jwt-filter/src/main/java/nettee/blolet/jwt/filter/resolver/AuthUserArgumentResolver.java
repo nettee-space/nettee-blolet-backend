bd48c5a3805ff05d1cd6f4ace8fe784d0843aeb4
merge-simpson <letsdev@kakao.com>
2025-08-31 23:27:53 +0900

    feat(jwt): support optional authorized user injection in `AuthUserArgumentResolver`
    Enhanced `AuthUserArgumentResolver` to handle both `AuthorizedUser` and `Optional<AuthorizedUser>` parameter types.
    Added early return check for annotation presence and generic type resolution with `ResolvableType`.
    
    `@AuthUser` 파라미터를 `Optional<AuthorizedUser>` 형태로도 주입할 수 있도록 기능을 확장했습니다.
    애노테이션 체크에 얼리리턴을 추가하고, 제네릭 타입을 안전하게 해석합니다.

--- core/security/jwt/blolet-jwt-filter/src/main/java/nettee/blolet/jwt/filter/resolver/AuthUserArgumentResolver.java
+++ core/security/jwt/blolet-jwt-filter/src/main/java/nettee/blolet/jwt/filter/resolver/AuthUserArgumentResolver.java
@@ -5,11 +5,14 @@ import lombok.RequiredArgsConstructor;
 import nettee.blolet.jwt.filter.annotation.AuthUser;
 import nettee.blolet.jwt.filter.annotation.AuthorizedUser;
 import org.springframework.core.MethodParameter;
+import org.springframework.core.ResolvableType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Optional;
 
 /**
  * @AuthUser 어노테이션이 붙은 파라미터를 처리하여 AuthorizedUser 객체를 주입하는 ArgumentResolver
@@ -20,8 +23,22 @@ public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(AuthUser.class)
-                && parameter.getParameterType().equals(AuthorizedUser.class);
+        // 얼리리턴
+        if (!parameter.hasParameterAnnotation(AuthUser.class)) {
+            return false;
+        }
+        // AuthorizedUser 직접 타입
+        if (parameter.getParameterType().equals(AuthorizedUser.class)) {
+            return true;
+        }
+
+        // Optional<AuthorizedUser> 제네릭 타입
+        if (parameter.getParameterType().equals(Optional.class)) {
+            Class<?> genericType = ResolvableType.forMethodParameter(parameter).getGeneric(0).resolve();
+            return AuthorizedUser.class.equals(genericType);
+        }
+
+        return false;
     }
 
     @Override
@@ -32,7 +49,12 @@ public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
             WebDataBinderFactory binderFactory
     ) {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        return request.getAttribute(AuthorizedUser.class.getTypeName());
+        var user = request.getAttribute(AuthorizedUser.class.getTypeName());
+
+        if (parameter.getParameterType().equals(Optional.class)) {
+            return Optional.ofNullable(user);
+        }
+
+        return user;
     }
 }
-
