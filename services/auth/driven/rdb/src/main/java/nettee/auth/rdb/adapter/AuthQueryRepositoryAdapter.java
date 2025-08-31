408923fee382fa4bccb3a61862390f524ea5127f
wch-os <wch1224.s@gmail.com>
2025-08-31 20:35:01 +0900

    feat(auth): implement `updatePasswordByEmail` in port adapter

--- services/auth/driven/rdb/src/main/java/nettee/auth/rdb/adapter/AuthQueryRepositoryAdapter.java
+++ services/auth/driven/rdb/src/main/java/nettee/auth/rdb/adapter/AuthQueryRepositoryAdapter.java
@@ -26,4 +26,10 @@ public class AuthQueryRepositoryAdapter implements AuthQueryRepositoryPort {
     public boolean existsByLoginId(String loginId) {
         return authJpaRepository.existsByLoginId(loginId);
     }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        Optional<UserEntity> userEntity = authJpaRepository.findByEmail(email);
+        return userEntity.map(mapper::toDomain);
+    }
 }
