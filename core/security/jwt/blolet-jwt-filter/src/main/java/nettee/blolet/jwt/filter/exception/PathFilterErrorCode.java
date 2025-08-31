7e05dd77f98caae86b54ec8a27d0d373918575ca
merge-simpson <letsdev@kakao.com>
2025-08-31 20:55:17 +0900

    feat(security): add `AUTH_ACCESS_TOKEN_REQUIRED` error code in auth filter error code
    Introduce `AUTH_ACCESS_TOKEN_REQUIRED` to `PathFilterErrorCode` enum with `UNAUTHORIZED` status.
    This ensures clear handling when access token is missing in request.
    
    JWT 인가 필터의 `PathFilterErrorCode` 열거형에 `AUTH_ACCESS_TOKEN_REQUIRED` 상수를 추가했습니다.
    요청에 액세스 토큰이 누락된 경우 `UNAUTHORIZED` 상태를 명확하게 처리할 수 있습니다.

--- core/security/jwt/blolet-jwt-filter/src/main/java/nettee/blolet/jwt/filter/exception/PathFilterErrorCode.java
+++ core/security/jwt/blolet-jwt-filter/src/main/java/nettee/blolet/jwt/filter/exception/PathFilterErrorCode.java
@@ -7,7 +7,8 @@ import java.util.Map;
 import java.util.function.Supplier;
 
 public enum PathFilterErrorCode implements ErrorCode {
-    AUTH_FILTER_PATH_NOT_ENTERED(HttpStatus.INTERNAL_SERVER_ERROR, "경로를 입력해야 합니다.")
+    AUTH_FILTER_PATH_NOT_ENTERED(HttpStatus.INTERNAL_SERVER_ERROR, "경로를 입력해야 합니다."),
+    AUTH_ACCESS_TOKEN_REQUIRED(HttpStatus.UNAUTHORIZED, "액세스 토큰이 입력되지 않습니다.")
     ;
 
     private final HttpStatus httpStatus;
