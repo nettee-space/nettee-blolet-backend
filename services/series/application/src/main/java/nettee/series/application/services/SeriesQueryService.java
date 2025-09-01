360b6a6f7d8caa574a6cb1514575cae223b687a6
merge-simpson <letsdev@kakao.com>
2025-09-02 00:49:40 +0900

    feat(series): implement `SeriesVisitUseCase` in series query service

--- services/series/application/src/main/java/nettee/series/application/services/SeriesQueryService.java
+++ services/series/application/src/main/java/nettee/series/application/services/SeriesQueryService.java
@@ -3,6 +3,7 @@ package nettee.series.application.services;
 import lombok.RequiredArgsConstructor;
 import nettee.series.application.port.SeriesQueryRepositoryPort;
 import nettee.series.application.usecase.SeriesReadUseCase;
+import nettee.series.application.usecase.SeriesVisitUseCase;
 import nettee.series.readmodel.SeriesQueryModels.SeriesDetail;
 import nettee.series.readmodel.SeriesQueryModels.SeriesSummary;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,7 @@ import static nettee.series.exception.SeriesErrorCode.SERIES_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
-public class SeriesQueryService implements SeriesReadUseCase {
+public class SeriesQueryService implements SeriesReadUseCase, SeriesVisitUseCase {
     
     private final SeriesQueryRepositoryPort queryRepositoryPort;
     
@@ -30,5 +31,11 @@ public class SeriesQueryService implements SeriesReadUseCase {
         assert blogId != null;
         
         return queryRepositoryPort.findAllByBlogId(blogId);
+    }
+
+    @Override
+    public SeriesDetail visitSeries(String seriesId) {
+        return queryRepositoryPort.findExceptDraftsBySeriesId(seriesId)
+                .orElseThrow(SERIES_NOT_FOUND::exception);
     }
 }
