239006c37be564257fecc9af7bd421afb714b41a
merge-simpson <letsdev@kakao.com>
2025-09-01 23:38:14 +0900

    feat(blog): add jpa repository method to query blog ids by user id

--- services/blog/driven/rdb/src/main/java/nettee/blolet/blog/rdb/repository/BlogQueryJpaRepository.java
+++ services/blog/driven/rdb/src/main/java/nettee/blolet/blog/rdb/repository/BlogQueryJpaRepository.java
@@ -1,7 +1,11 @@
 package nettee.blolet.blog.rdb.repository;
 
 import nettee.blolet.blog.rdb.entity.BlogEntity;
+import nettee.blolet.blog.rdb.repository.projection.BlogQueryProjection.BlogIdProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface BlogQueryJpaRepository extends JpaRepository<BlogEntity, Long> {
+    Set<BlogIdProjection> findBlogIdByUserId(Long userId);
 }
