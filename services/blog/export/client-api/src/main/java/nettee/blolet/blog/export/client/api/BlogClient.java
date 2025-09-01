f173d074844855b6f1844099417ebbb4bd9e090b
merge-simpson <letsdev@kakao.com>
2025-09-01 23:41:57 +0900

    feat(blog): introduce `getBlogIdsByUserId` in blog client

--- services/blog/export/client-api/src/main/java/nettee/blolet/blog/export/client/api/BlogClient.java
+++ services/blog/export/client-api/src/main/java/nettee/blolet/blog/export/client/api/BlogClient.java
@@ -1,14 +1,17 @@
 package nettee.blolet.blog.export.client.api;
 
+import nettee.blolet.blog.export.client.api.BlogClientDto.BlogIdsQueryResponse;
 import nettee.blolet.blog.export.client.api.BlogClientDto.BlogInternalCreateCommand;
 import nettee.blolet.blog.export.client.api.BlogClientDto.BlogCreateResponse;
 import nettee.blolet.blog.export.client.api.BlogClientDto.BlogOwnershipVerifyResponse;
 
 public interface BlogClient {
+    BlogCreateResponse create(BlogInternalCreateCommand dto);
+
+    BlogIdsQueryResponse getBlogIdsByUserId(String userId);
+
     BlogOwnershipVerifyResponse verifyOwnership(String userId, String blogId);
     BlogOwnershipVerifyResponse verifyOwnershipFresh(String userId, String blogId);
     BlogOwnershipVerifyResponse verifyOwnershipByProfileId(String profileId, String blogId);
     BlogOwnershipVerifyResponse verifyOwnershipByProfileIdFresh(String profileId, String blogId);
-
-    BlogCreateResponse create(BlogInternalCreateCommand dto);
 }
