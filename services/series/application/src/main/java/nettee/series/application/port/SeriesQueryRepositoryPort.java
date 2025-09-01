a1e1970d731472003a3ab92212c22d30aba92ecc
merge-simpson <letsdev@kakao.com>
2025-09-02 01:13:17 +0900

    feat(series): introduce `findExceptDraftsById` in detail query repository port

--- services/series/application/src/main/java/nettee/series/application/port/SeriesQueryRepositoryPort.java
+++ services/series/application/src/main/java/nettee/series/application/port/SeriesQueryRepositoryPort.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 
 public interface SeriesQueryRepositoryPort {
     
-    
     Optional<SeriesDetail> findBySeriesIdAndOwnership(String seriesId, String userBlogId);
+    Optional<SeriesDetail> findExceptDraftsById(String seriesId);
+
     List<SeriesSummary> findAllByBlogId(String blogId);
 }
