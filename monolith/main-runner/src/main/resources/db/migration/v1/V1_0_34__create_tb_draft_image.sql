d6002503681f89035d8bc1f614d646cb6f8862f6
전상은 <wowddok99@gmail.com>
2025-09-07 23:07:45 +0900

    feat(flyway): add banner_image_id and scheduled_deploy_at columns to draft table

--- monolith/main-runner/src/main/resources/db/migration/v1/V1_0_34__create_tb_draft_image.sql
+++ monolith/main-runner/src/main/resources/db/migration/v1/V1_0_34__create_tb_draft_image.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS article.draft_image (
-                                                   id          BIGINT,
-                                                   image_url   TEXT        NOT NULL,
-                                                   created_at  TIMESTAMP   DEFAULT NOW()   NOT NULL
-    );
+    id          BIGINT,
+    image_url   TEXT        NOT NULL,
+    created_at  TIMESTAMP   DEFAULT NOW()   NOT NULL,
+
+    CONSTRAINT "pk_draft__image" PRIMARY KEY ("id"),
+    ADD CONSTRAINT "uq_draft_image__url" UNIQUE ("image_url")
+);
 
 -- 컬럼 코멘트
 COMMENT ON COLUMN article.draft_image.id        IS '이미지 PK';
 COMMENT ON COLUMN article.draft_image.image_url IS '이미지 URL';
 COMMENT ON COLUMN article.draft_image.created_at IS '등록 시간';
-
-ALTER TABLE "article"."draft_image" ADD CONSTRAINT "pk_draft_image" PRIMARY KEY ("id");
-ALTER TABLE "article"."draft_image" ADD CONSTRAINT "uq_draft_image_url" UNIQUE ("image_url");
