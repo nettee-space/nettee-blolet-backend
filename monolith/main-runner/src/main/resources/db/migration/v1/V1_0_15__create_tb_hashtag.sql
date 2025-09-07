f9e3ba2a8846eb2fcd18151c8cc6bb0404f8911f
전상은 <wowddok99@gmail.com>
2025-09-08 00:19:47 +0900

    chore(flyway): add missing `article.hashtag` table

---
+++ monolith/main-runner/src/main/resources/db/migration/v1/V1_0_15__create_tb_hashtag.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA IF NOT EXISTS "article";
+
+CREATE TABLE IF NOT EXISTS article.hashtag (
+    id          BIGSERIAL,
+    name        VARCHAR(100) NOT NULL,
+    count       INTEGER DEFAULT 1,
+
+    created_at  TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at  TIMESTAMP DEFAULT NOW() NOT NULL,
+
+    CONSTRAINT pk_hashtag PRIMARY KEY (id),
+    CONSTRAINT uq_hashtag__name UNIQUE (name)
+);
+
+-- 컬럼 코멘트
+COMMENT ON COLUMN article.hashtag.id         IS '해시태그 PK';
+COMMENT ON COLUMN article.hashtag.name       IS '해시태그 이름';
+COMMENT ON COLUMN article.hashtag.count      IS '사용 횟수';
+COMMENT ON COLUMN article.hashtag.created_at IS '생성시간';
+COMMENT ON COLUMN article.hashtag.updated_at IS '수정시간';
