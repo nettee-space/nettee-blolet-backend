6554cfb9ec67746d7a4bee5ba62cdc3fe1a0934f
wch-os <wch1224.s@gmail.com>
2025-09-13 00:43:54 +0900

    refactor(flyway): remove previous profile table creation script
    - profile 테이블 정의를 간소화하여, 이전 profile 테이블 생성 스크립트를 삭제함

--- monolith/main-runner/src/main/resources/db/migration/dummy/R__init_dummies.sql
+++ monolith/main-runner/src/main/resources/db/migration/dummy/R__init_dummies.sql
@@ -46,41 +46,6 @@ INSERT INTO "auth"."user" (
     'moon@gmail.com'
 );
 
-INSERT INTO "profile"."profile" (
-    "id",
-    "user_id",
-    "nickname",
-    "gender",
-    "birth",
-    "profile_url",
-    "bio",
-    "tel",
-    "occupation",
-    "interest"
-) VALUES (
-    current_setting('dummy.PROFILE_ID')::bigint,
-    current_setting('dummy.USER_ID')::bigint,
-    '동굴',
-    'NOT_THAT_SIMPLE',
-    '2025-08-15'::date,
-    'https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-2228-622f-a21e-41f365335041/raw',
-    '매너 있고 다정한 ^^;;; 젊은 남자 개발자입니다. 여친 있음 ^^;',
-    '010-1234-1234',
-    '백엔드 엔지니어',
-    '서울 데이트 코스,공부하기 좋은 카페,느좋카'
-), (
-    current_setting('dummy.PROFILE_ID2')::bigint,
-    current_setting('dummy.USER_ID2')::bigint,
-    '침착너구리',
-    '안드로진뭐시기',
-    '2025-08-15'::date,
-    'https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-2228-622f-a21e-41f365335041/raw',
-    '젊은 남자 개발자입니다. 남친 있음 ^^;',
-    '010-1234-1235',
-    '프론트엔드 엔지니어',
-    '전국 데이트 코스,공부하기 좋은 카페,느좋카'
-);
-
 INSERT INTO "blog"."blog" (
     "id",
     "user_id",
