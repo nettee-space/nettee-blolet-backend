749d2df89f7b39d60c189a5a5ec0f9998fb73978
wch-os <wch1224.s@gmail.com>
2025-09-17 22:41:11 +0900

    feat(auth): call profile API for signup and login flows
    - 회원가입, 로그인 시 프로필 생성 및 조회 API 호출
    - 발급되는 jwt 토큰 클레임에는 profileId가 포함되도록 수정
    - 신규 가입자의 경우, 프로필이 자동으로 생성

--- services/auth/application/build.gradle.kts
+++ services/auth/application/build.gradle.kts
@@ -1,9 +1,13 @@
 val authApi: String by project
+val profileReadModel: String by project
 
 dependencies {
     api(project(authApi))
     api(project(":security-password"))
     api(project(":security-jwt-issuer"))
+    implementation(project(":client-api"))
+    implementation(project(":rest-client"))
+    implementation(project(profileReadModel))
 
     implementation("org.springframework.boot:spring-boot-starter-mail")
 }
